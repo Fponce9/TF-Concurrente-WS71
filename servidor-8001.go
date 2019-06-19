8ccfc57912df0a88fef33a4f57a05cd0e3dc7d1e
Fernando <exminaruto@gmail.com>
2019-06-19 09:03:03 -0500

    Servidor y Cliente 8001

--- servidor-8001.go
+++ servidor-8001.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 )
 
 func main() {
-	fmt.Print("GAAA")
+	ln, _ := net.Listen("tcp", "localhost:8001")
+	defer ln.Close()
+	con, _ := ln.Accept()
+	defer con.Close()
+	ips := []string{"192.168.1.10", "192.168.1.20", "192.168.1.30"}
+	r := bufio.NewReader(con)
+	IP, _ := r.ReadString('\n')
+	ips = append(ips, IP)
+	fmt.Println(IP)
 }
